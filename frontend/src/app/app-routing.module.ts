b8891943fa57d1ae10e2f42739525746ac9d9f56
vipin_760 <vipinm500@gmail.com>
2024-01-06 12:03:03 +0530

    setup login and register and home pages also configure their routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/pages/login/login.component';
+import { RegisterComponent } from './components/pages/register/register.component';
+import { HomeComponent } from './components/pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login', component:LoginComponent},
+  {path:'register', component:RegisterComponent},
+  {path:'home',component:HomeComponent},
+  {path:'',redirectTo:'/login', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
+import { RegisterComponent } from './components/pages/register/register.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { HeaderComponent } from './components/partias/header/header.component';
+import { FooterComponent } from './components/partias/footer/footer.component';
+import { HomeComponent } from './components/pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    RegisterComponent,
     LoginComponent,
-    RegisterComponent
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
